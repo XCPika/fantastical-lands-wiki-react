1217c01a685efbe87d8ad72f126c33e54569a7cf
XCPika <61687817+XCPika@users.noreply.github.com>
2023-08-09 16:48:23 +0100

    Made nav more inline with react philosophy

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import NavItem from './nav-item';
+import React, { Children, cloneElement } from 'react';
 import './nav.scss';
-import class_logo from './img/title_classes.png'
-import race_logo from './img/title_races.png'
-import item_logo from './img/title_items.png'
-import structure_logo from './img/title_structures.png'
 import nav_icon_logo from './img/nav-bar-icon.png'
 import nav_title_logo from './img/title_fantastical_lands.png'
 import StoneBorder from '../stone_border';
 import { Link } from 'react-router-dom';
 
-export default function Nav() {
-
+export default function Nav(props) {
+    const arrayChildren = Children.toArray(props.children);
+    const leftChildren = arrayChildren.slice(0, 2);
+    const rightChildren = arrayChildren.slice(2, 4);
+    
     return (
         <header>
             <nav>
                 <div className="nav-left">
-                    <NavItem icon={class_logo} title="Classes" route='/classes'/>
-                    <NavItem icon={race_logo} title="Races" route='/races'/>
+                    {leftChildren}
                 </div>
                 <Link to='/' className='nav-center'>
                     <img 
@@ -32,8 +29,7 @@ export default function Nav() {
                     ></img>
                 </Link>
                 <div className="nav-right">
-                    <NavItem icon={item_logo} title="Items" route='/items'/>
-                    <NavItem icon={structure_logo} title="Structures" route='/structures'/>
+                    {rightChildren}
                 </div>
             </nav>
             <StoneBorder/>
--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Nav from "../../components/nav";
-import dark_bg from './img/background-dark.png'
-import bg from './img/background.png'
+import NavItem from '../../components/nav-item';
+import dark_bg from './img/background-dark.png';
+import bg from './img/background.png';
+import class_logo from './img/title_classes.png';
+import race_logo from './img/title_races.png';
+import item_logo from './img/title_items.png';
+import structure_logo from './img/title_structures.png';
 
 
 const Main = styled.div`
@@ -19,7 +24,12 @@ export default function Layout() {
 
     return (
         <Main dark_mode={false}>
-            <Nav/>
+            <Nav>
+                <NavItem icon={class_logo} title="Classes" route='/classes'/>
+                <NavItem icon={race_logo} title="Races" route='/races'/>
+                <NavItem icon={item_logo} title="Items" route='/items'/>
+                <NavItem icon={structure_logo} title="Structures" route='/structures'/>
+            </Nav>
             <Outlet />
         </Main>
     )
